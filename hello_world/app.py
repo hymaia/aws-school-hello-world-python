8fa8339c67c3087388974245f74e652bf2cec79a
Franck Cussac <franck_cussac@ext-connect.sncf>
2023-05-09 19:26:11 +0200

    test deploy

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "salut c'est l'aws school, on va tout déchirer",
             # "location": ip.text.replace("\n", "")
         }),
     }
